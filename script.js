a12856cb918179650e0f10fde9996bfe2c5408de
py4object <py4object@gmail.com>
2016-04-14 03:03:36 +0300

    added support for groups

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-clearColor = '#c3ffff'
+clearColor = '#EDEDED'//'#c3ffff'
 brownColor = '#aa7941'
 stoneColor1 = '#a0a0a0'
 
@@ -21,36 +21,36 @@ TWorld.prototype.loadImage=function(){
     //this.land.update();
     //this.land.addToWorld();
     
-	var img = game.cache.getImage('land');
+	this.img = game.cache.getImage('land');
     var canvas = document.createElement('canvas');
-    canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
-    var pixelData = canvas.getContext('2d').getImageData(0, 0, img.width, img.height).data;
+    canvas.getContext('2d').drawImage(this.img, 0, 0, this.img.width, this.img.height);
+    var pixelData = canvas.getContext('2d').getImageData(0, 0, this.img.width, this.img.height).data;
    // document.body.appendChild(canvas);
    // Phaser.Canvas.addToDOM(canvas, parent, overflowHidden)
     var sprite = game.add.sprite(0, 0, this.land)
-	var pixelData=canvas.getContext('2d').getImageData(0,0,img.width,img.height).data;
-	this.data=new Array(img.width);
-	this.width=600/this.tileSize;
-	this.height=800/this.tileSize;
+	var pixelData=canvas.getContext('2d').getImageData(0,0,this.img.width,this.img.height).data;
+	this.data=new Array(this.img.width);
+	this.width=800/this.tileSize;
+	this.height=600/this.tileSize;
 	
 
 	
-	this.futureData=new Array(img.height);
-	for (var x = 0; x< img.width; x++) {
-		this.data[x]=new Array(img.width);
-		this.futureData[x]=new Array(img.width);
-		for(var y =0;y<img.height;y++){
-			var red = pixelData[((img.width * y) + x) * 4]
-			var  green = pixelData[((img.width * y) + x) * 4 + 1]
-			var  blue = pixelData[((img.width * y) + x) * 4 + 2]
-			var alpha = pixelData[((img.width * y) + x) * 4 + 3]
+	this.futureData=new Array(this.img.height);
+	for (var x = 0; x< this.img.width; x++) {
+		this.data[x]=new Array(this.img.width);
+		this.futureData[x]=new Array(this.img.width);
+		for(var y =0;y<this.img.height;y++){
+			var red = pixelData[((this.img.width * y) + x) * 4]
+			var  green = pixelData[((this.img.width * y) + x) * 4 + 1]
+			var  blue = pixelData[((this.img.width * y) + x) * 4 + 2]
+			var alpha = pixelData[((this.img.width * y) + x) * 4 + 3]
 			var hex = Helper.rgbToHex(red, green, blue);
 			this.data[x][y]=hex;
 			this.futureData[x][y]=hex;
 			this.land.ctx.fillStyle = hex
    			this.land.ctx.fillRect(4 * x, 4 * y, 4, 4)
    			this.land.dirty=true;
-   			//console.log(hex);
+   			////console.log(hex);
 		}	
 
 	}
@@ -62,7 +62,7 @@ TWorld.prototype.loadImage=function(){
 }
 TWorld.prototype.x=10;
 TWorld.prototype.isWalkable=function(hexColor){
-	console.log(hexColor +" "+ clearColor);
+	//console.log(hexColor +" "+ clearColor);
 	return hexColor  ==clearColor;
 }
 
@@ -74,16 +74,19 @@ TWorld.prototype.render=function(force=false){
 				this.data[x][y]=this.futureData[x][y];
 				this.drawTile(this.data[x][y],x,y);
 				this.land.dirty=true;;
-				console.log("renderd");
+				//console.log("renderd");
 			}	
 		}
 	}
+	
 
 }
 
 TWorld.prototype.drawTile=function(color,x,y){
+
 	this.land.ctx.fillStyle = color
-   	this.land.ctx.fillRect(4 * x, 4 * y, 4, 4)
+
+   	this.land.ctx.clearRect(4 * x, 4 * y, 4, 4)
 
 }
 
@@ -98,6 +101,22 @@ TWorld.prototype.highestNonFreeTile=function(x,y){
 	return cy;
 
 }
+
+TWorld.prototype.highestTile=function(x,y){
+	var cy=y;
+	while (cy>=0){
+		if(!this.isWalkable(this.data[x][y]))
+			break;
+		else
+			cy--;
+	}
+	return cy;
+
+}
+
+
+
+
 
 TWorld.prototype.TileForWorld=function(world){
 	var result=world/this.tileSize;
@@ -116,8 +135,7 @@ TWorld.prototype.destoryCircle=function(r,tileX,tileY){
 				
 				this.futureData[darwTileX][darwTileY]=clearColor;
 				this.land.dirty=true;
-				
-				console.log("destroed");
+				//console.log("destroed");
 			}
 		}
 	}
@@ -125,17 +143,17 @@ TWorld.prototype.destoryCircle=function(r,tileX,tileY){
 }
 
 
-function PhysicsWorld(game,TWorld,charcter){
+function PhysicsWorld(game,TWorld,charcters){
 	this.game=game;
-	this.TWorld=TWorld;
-	this.charcter=charcter;
-	console.log(charcter);
+	this.World=TWorld;
+	this.charcters=charcters;
+	//console.log(charcter);
 }
 PhysicsWorld.prototype=Phaser.Plugin
 
 PhysicsWorld.prototype.update=function(){
 
-	this.checkCharcterCollison (this.charcter);
+	this.charcters.forEach(this.checkCharcterCollison);
 
 }
 
@@ -145,53 +163,72 @@ PhysicsWorld.prototype.postUpdate=function(){
 
 PhysicsWorld.prototype.checkCharcterCollison=function(charcter){
 
-	var tileX = this.TWorld.TileForWorld(this.charcter.body.x + (this.charcter.width/2 ))
-    tileX = Phaser.Math.clamp(tileX, 0, this.TWorld.width-1)
-    var tileY = this.TWorld.TileForWorld(this.charcter.body.y + this.charcter.height)
-    tileY = Phaser.Math.clamp(tileY, 0, this.TWorld.height-1)
-    tileHexColor = this.TWorld.data[tileX][tileY]
+	var tileX = this.World.TileForWorld(charcter.x +(charcter.width/2))
+    tileX = Phaser.Math.clamp(tileX, 0, this.World.width-1)
+    var tileY = this.World.TileForWorld(charcter.body.y + charcter.height)
+    var tileTopY=this.World.TileForWorld(charcter.body.y);
+    tileY = Phaser.Math.clamp(tileY, 0, this.World.height-1);
+    tileTopY=Phaser.Math.clamp(tileTopY, 0, this.World.height-1);
 
-		console.log(tileX+" "+tileY)
-	// if(!this.TWorld.isWalkable(this.TWorld.data[tileX+1][tileY-1])){
+    tileHexColor = this.World.data[tileX][tileY]
+
+		//console.log(tileX+" "+tileY)
+	// if(!this.World.isWalkable(this.World.data[tileX+1][tileY-1])){
 	// 	charcter.body.velocity=0;
-	// 	console.log("1");
+	// 	//console.log("1");
 
 	// }
-	//  if(!this.TWorld.isWalkable(this.TWorld.data[tileX-1][tileY-1])){
+	//  if(!this.World.isWalkable(this.World.data[tileX-1][tileY-1])){
 	// 	charcter.body.velocity=0;
-	// 	console.log("2");
+	// 	//console.log("2");
 	// }
 	// 
 	// 
 	// 
 	// 
-	if(!this.TWorld.isWalkable(this.TWorld.data[tileX+1][tileY-1])||
-		!this.TWorld.isWalkable(this.TWorld.data[tileX+2][tileY-1])||
-		!this.TWorld.isWalkable(this.TWorld.data[tileX+3][tileY-1])){
+	if(!this.World.isWalkable(this.World.data[tileX+1][tileY-1])||
+		!this.World.isWalkable(this.World.data[tileX+2][tileY-1])||
+		!this.World.isWalkable(this.World.data[tileX+3][tileY-1])){
 			if(charcter.body.velocity.x>0){
 				charcter.body.velocity.x=0;
-			console.log('cant go right');
+			//console.log('cant go right');
 			}
 	}
-	if(!this.TWorld.isWalkable(this.TWorld.data[tileX-1][tileY-1])||
-		!this.TWorld.isWalkable(this.TWorld.data[tileX-2][tileY-1])||
-		!this.TWorld.isWalkable(this.TWorld.data[tileX-3][tileY-1])){
+	if(!this.World.isWalkable(this.World.data[tileX-1][tileY-1])||
+		!this.World.isWalkable(this.World.data[tileX-2][tileY-1])||
+		!this.World.isWalkable(this.World.data[tileX-3][tileY-1])){
 			if(charcter.body.velocity.x<0){
 				charcter.body.velocity.x=0;
-			console.log('cant go left');
+			//console.log('cant go left');
 			}
 	}
+
+	if(charcter.body.velocity.y<0){
+		//console.log("1");
+		if(!this.World.isWalkable(this.World.data[tileX][tileTopY])){
+			charcter.body.velocity.y=0;
+			var highestTire=this.World.highestTile(tileX, tileTopY);
+		//	console.log("2");
+			charcter.body.y=highestTire*4+1;
+		}
+		
+
+	}
+
 	
-	if(this.TWorld.isWalkable(tileHexColor)){
+	if(this.World.isWalkable(tileHexColor)){
 		//charcter.body.velocity.x=50;
 		charcter.body.acceleration.y=100;
-	}
-	 
-	else{
+	
+}	
+
+	else if(charcter.body.velocity.y>=0){
 		charcter.body.acceleration.y=0;
 		charcter.body.velocity.y=0;
 		//charcter.body.velocity.x=0;
-		var highestTire=this.TWorld.highestNonFreeTile(tileX, tileY);
+		//
+		//
+		var highestTire=this.World.highestNonFreeTile(tileX, tileY);
 		if( highestTire>tileY-5){
 			charcter.body.y=highestTire*4-charcter.height+1;
 		}else{
